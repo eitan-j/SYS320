e7e13aa6a769d81edd9a17e001c1c7c4def1a1f8
eitan-j <47335082+eitan-j@users.noreply.github.com>
2023-03-21 16:34:16 -0400

    week 8 done

--- Week8/getlogs.ps1
+++ Week8/getlogs.ps1
@@ -1,7 +1,6 @@
 ﻿# Storyline: Review the Security Event Log
 
 # Directory to save files:
-
 $myDir = ".\"
 
 # List all the available Windows Event logs
@@ -10,8 +9,12 @@ Get-EventLog -List
 # Create a prompt to allow user to select the Log to view
 $readLog = Read-Host -Prompt "Please select a log to review from the list above"
 
+# Create a prompt to allow user to select the keyword to search for
+$findMessage = Read-Host -Prompt "Please enter a keyword or phrase to search for"
+
+
 # Print the results for the log
-Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*new process has been*" } | export-csv -NoTypeInformation `
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$findMessage*" } | export-csv -NoTypeInformation `
 -Path "$myDir\securityLogs.csv"
 
 # Task: Create a prompt that allows the user to specify a keyword or phrase to search on.
