bc65a8d64abf7d0ff290c28b14375acd0460c0c0
eitan-j <47335082+eitan-j@users.noreply.github.com>
2023-03-21 15:56:56 -0700

    week 8 videos finished

---
+++ Week8/getlogs.ps1
@@ -0,0 +1,18 @@
+﻿# Storyline: Review the Security Event Log
+
+# Directory to save files:
+
+$myDir = ".\"
+
+# List all the available Windows Event logs
+Get-EventLog -List
+
+# Create a prompt to allow user to select the Log to view
+$readLog = Read-Host -Prompt "Please select a log to review from the list above"
+
+# Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*new process has been*" } | export-csv -NoTypeInformation `
+-Path "$myDir\securityLogs.csv"
+
+# Task: Create a prompt that allows the user to specify a keyword or phrase to search on.
+# Find a string from your event logs to search on
---
+++ Week8/sendemail.ps1
@@ -0,0 +1,18 @@
+﻿# Storyline: Send an email.
+
+# Variable can have an underscore or any alphanumeric value.
+
+# Body of the email
+$msg = "Hello there."
+
+# echoing to the screen.
+write-host -BackgroundColor Red -ForegroundColor White $msg
+
+# Email From Address
+$email = "eitan.jacobsohn@mymail.champlain.edu"
+
+# To address
+$toEmail = "deployer@csi-web"
+
+# Sending the email
+Send-MailMessage -From $email -To $toEmail -Subject "A Greeting" -Body $msg -SmtpServer 192.168.6.71
