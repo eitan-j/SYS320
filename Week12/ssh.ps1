534d574138b4793233ffa7cdb341dae894a4373f
eitan-j <47335082+eitan-j@users.noreply.github.com>
2023-04-13 13:00:32 -0400

    week 12 video done

---
+++ Week12/ssh.ps1
@@ -0,0 +1,19 @@
+﻿# Login to a remote SSH server
+New-SSHSession -ComputerName '192.168.4.22' -Credential (Get-Credential sys320)
+
+
+while ($true) {
+
+    # Add a prompt to run commands
+    $the_cmd = Read-Host -Prompt "Please enter a command"
+
+    # Run command on remote SSH server
+    (Invoke-SSHCommand -Index 0 $the_cmd).Output
+
+}
+
+Remove-SSHSession -SessionId 0
+
+
+# Set-SCPItem -ComputerName '192.168.4.22' -Credential (Get-Credential sys320) `
+# -Path '.\tedx.jpeg' -Destination '/home/sys320'
