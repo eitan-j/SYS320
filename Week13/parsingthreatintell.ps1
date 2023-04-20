3064ff5f8a5c00d0c138ee6c99bb43208fd2ffc7
eitan-j <47335082+eitan-j@users.noreply.github.com>
2023-04-20 19:46:45 -0400

    week 13 video done

---
+++ Week13/parsingthreatintell.ps1
@@ -0,0 +1,45 @@
+﻿# Array of websites containing threat intell
+$drop_urls = @('https://rules.emergingthreats.net/blockrules/emerging-botcc.rules','https://rules.emergingthreats.net/blockrules/compromised-ips.txt')
+
+# Loop through the URLs for the rules list
+foreach ($u in $drop_urls) {
+
+    # Extract the filename
+    $temp = $u.split('/')
+    
+    # The last element in the array plucked off is the filename
+    $file_name = $temp[-1]
+
+    if (Test-Path $file_name) {
+
+        continue
+
+    } else {
+     
+        # Download the rules list
+        Invoke-WebRequest -Uri $u -OutFile $file_name -UseBasicParsing
+
+    } # Close if statement
+
+} # Close the foreach loop
+
+# Array containing the filename
+$input_paths = @('.\emerging-botcc.rules','.\compromised-ips.txt')
+
+# Extract the IP addresses
+# 192.168.0.1
+
+$regex_drop = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+
+# Append the IP addresses to the temporary IP list
+Select-String -Path $input_paths -Pattern $regex_drop | `
+ForEach-Object { $_.Matches.Value } | Sort-Object | Get-Unique | `
+Out-File -FilePath 'ips-bad.tmp'
+
+
+# Get the IP addresses discovered, loop through and replace the beginning of the line with the IPTables syntax
+# After the IP address, add the remaining IPTables syntax and save the results to a file
+# iptables -A INPUT -s 192.168.0.1 -j DROP
+(Get-Content -Path '.\ips-bad.tmp') | ForEach-Object `
+{ $_ -replace '^','iptables -A INPUT -s ' -replace '$',' -j DROP'} | `
+Out-File -FilePath 'iptables.bash'
